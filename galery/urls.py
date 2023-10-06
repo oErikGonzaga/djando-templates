4b50022f051aecf9aa7f44bfabf88f4b316a4260
Erik Gonzaga <92276557+oErikGonzaga@users.noreply.github.com>
2023-10-06 03:26:40 -0300

    ComentÃarios

--- galery/urls.py
+++ galery/urls.py
@@ -1,7 +1,14 @@
+# Importa o módulo 'path' de 'django.urls'
 from django.urls import path
+
+# Importa as funções 'index' e 'image' do módulo 'views' em 'galery'
 from galery.views import index, image
 
+# URLs da aplicação 'galery'
 urlpatterns = [
+    # Define a URL raiz ('/') para a função 'index' e nomeia-a como 'index'
     path('', index, name='index'),
+
+    # Define a URL 'image/' para a função 'image' e nomeia-a como 'image'
     path('image/', image, name='image')
 ]
--- galery/views.py
+++ galery/views.py
@@ -1,9 +1,12 @@
+# Importa o módulo 'render' de 'django.shortcuts'
 from django.shortcuts import render
 
 
+# Função para renderizar a página inicial
 def index(request):
     return render(request, 'galery/index.html')
 
 
+# Função para renderizar a página de imagem
 def image(request):
     return render(request, 'galery/image.html')
