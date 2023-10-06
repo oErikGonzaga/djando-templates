40f265bcb450f8bbbdbac212808d9d9caf31a317
Erik Gonzaga <92276557+oErikGonzaga@users.noreply.github.com>
2023-10-06 01:42:49 -0300

    Outras imagens e URL name

--- galery/urls.py
+++ galery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galery.views import index
+from galery.views import index, imagem
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
 ]
--- galery/views.py
+++ galery/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'galery/index.html')
+
+
+def imagem(request):
+    return render(request, 'galery/imagem.html')
